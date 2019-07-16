5c2ac50d849e0ccfaa782a1f4c1fe10a3f79a6c3
unknown <fraj.mohamed97@gmail.com>
2019-07-16 17:13:13 +0100

    first commit

---
+++ src/timer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Timer() {
+    return (
+        <div>
+            <h1 className='time'>00:30:00 </h1>
+            <h1 >Hour Minute Seconde</h1>
+        </div>
+    )
+}
+
+export default Timer;
